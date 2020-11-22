da6ff4c89ef8dc0bc970c02a3159c19f4e21c241
sylvestre <sylvestre.marie@gmail.com>
2020-11-22 18:16:58 +0100

    utilisateur --> sign up #4

---
+++ controllers/user.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const User = require("../models/user");
+
+let  userModels  = new User();
+
+exports.signup = (req, res, next) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    bcrypt.hash(password, 10)
+        .then(hash => {
+            let eltInsert = [email,hash,firstName,lastName];
+            userModels.signup(eltInsert)
+                .then((response) =>{
+                    res.status(201).json(JSON.stringify(response))
+                })
+                .catch((error) =>{
+                    console.error(error);
+                    res.status(400).json({error})
+                })
+        })
+        .catch(error => res.status(500).json({ error }));
+};
