882fa08d451178aa49e2d79603397e8c276fb898
sylvestre <sylvestre.marie@gmail.com>
2020-11-26 19:03:41 +0100

    article --> modification d'un article #11

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,10 +1,10 @@
 const db = require("../database_connect");
 const Articles = require("../models/articles");
+const fs = require('fs');
 
 let  ArticlesModels  = new Articles();
 
 exports.createArticle = (req, res, next) => {
-
     const article = JSON.parse(req.body.article);
     article.image = req.file.filename;
 
@@ -19,7 +19,18 @@ exports.createArticle = (req, res, next) => {
 };
 
 exports.modifyArticle = (req, res, next) => {
+    const articleToModify = req.file ?
+        {
+            ...JSON.parse(req.body.article),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+            filename: req.file.filename
+        } : {...req.body};
 
+    ArticlesModels.modify({_id: req.params.id}, {...articleToModify, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Post modifié !'}))
+        .catch((error) => {
+            res.status(400).json({error})
+        })
 };
 
 exports.deleteArticle = (req, res, next) => {
--- models/articles.js
+++ models/articles.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 const db = require("../database_connect");
 
-
 class ArticlesModels {
-        create(eltInsert) {
+    create(eltInsert) {
         let sql = 'INSERT INTO article(fk_id_user,titre_article,corps_article,image_article,date_article) VALUES (?,?,?,?,NOW())';
-        sql = mysql.format(sql,Object.values(eltInsert))
+        sql = mysql.format(sql, Object.values(eltInsert))
         return new Promise((resolve, reject) => {
             db.query(sql, function (err, result) {
                 if (err) reject({error: err});
@@ -13,5 +12,34 @@ class ArticlesModels {
             })
         })
     }
+
+    modify(id, body) {
+        let sql1 = 'SELECT * FROM article where id_article = ?';
+        sql1 = mysql.format(sql1, id._id);
+        return new Promise((resolve) => {
+            db.query(sql1, function (err, result) {
+                if (err) throw err;
+                if (body.userId == result[0].fk_id_user && body.filename == result[0].image_article) {
+                    let sql2 = 'UPDATE article SET titre_article = ?, corps_article = ? WHERE id_article = ? AND fk_id_user = ?';
+                    sql2 = mysql.format(sql2, [body.titre, body.corps, body._id, body.userId]);
+                    db.query(sql2, function (err, result, fields) {
+                        if (err) throw err;
+                        resolve({message: 'Post modifié !'});
+                    })
+                } else if (body.userId == result[0].fk_id_user && body.filename != result[0].image_article) {
+                    let sql3 = 'UPDATE article SET titre_article = ?, corps_article = ?, image_article = ?  WHERE id_article = ? AND fk_id_user = ?';
+                    sql3 = mysql.format(sql3, [body.titre, body.corps,body.filename, body._id, body.userId]);
+                    db.query(sql3, function (err, result, fields) {
+                        if (err) throw err;
+                        resolve({message: 'Post et image modifiés !'});
+                    })
+                } else {
+                    reject({error: 'fonction indisponible'});
+                }
+            })
+        });
+    }
+
+
 }
 module.exports = ArticlesModels;
