23d057a4a822d86e75782a33e0bd474b9f6aca57
sylvestre <sylvestre.marie@gmail.com>
2020-12-23 10:38:42 +0100

    user --> gestion utilisateur #18

--- controllers/user.js
+++ controllers/user.js
@@ -8,6 +8,7 @@ exports.signup = (req, res, next) => {
     let password = req.body.password;
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
+    console.log(req.body)
     bcrypt.hash(password, 10)
         .then(hash => {
             let eltInsert = [email,hash,firstName,lastName];
@@ -16,18 +17,15 @@ exports.signup = (req, res, next) => {
                     res.status(201).json(JSON.stringify(response))
                 })
                 .catch((error) =>{
-                    console.error(error);
                     res.status(400).json({error})
                 })
         })
         .catch(error => res.status(500).json({ error }));
 };
 
-
 exports.login = (req, res, next) => {
     let email = req.body.email;
     let password = req.body.password;
-
     userModels.login(email,password)
         .then((response) =>{
             res.status(200).json(JSON.stringify(response))
@@ -35,4 +33,27 @@ exports.login = (req, res, next) => {
         .catch((error) =>{
             res.status(400).json(error)
         })
+};
+exports.findOneUser = (req, res, next) => {
+    userModels.findOneUser({
+        _id: req.body.id_user
+    }).then((response) => {
+        res.status(200).json(response.result)
+    }).catch(
+        error => res.status(404).json({error: error})
+    );
+};
+
+exports.updateProfil = (req, res, next) => {
+    let id = req.body.id_user;
+    let email = req.body.email;
+    let firstname = req.body.firstName;
+    let lastname = req.body.lastName;
+    userModels.updateProfil(id,email,firstname,lastname)
+        .then((response) => {
+            res.status(200).json(response.result)
+        })
+        .catch(
+        error => res.status(404).json({error: error})
+    );
 };
