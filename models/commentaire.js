0c254a307d0b32bba11bdad404d2316ef0078e97
sylvestre <sylvestre.marie@gmail.com>
2020-12-23 09:31:16 +0100

    article --> ajout commentaire #17

--- models/commentaire.js
+++ models/commentaire.js
@@ -13,6 +13,39 @@ class CommentaireModels {
             })
         })
     }
-
+    findAll() {
+        let sql = 'SELECT * FROM commentaire';
+        sql = mysql.format(sql);
+        return new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject({error: err});
+                resolve({result})
+            })
+        })
+    }
+    findByArticleId(idArticle){
+        let sql = 'SELECT * FROM commentaire join user on user.id_user = commentaire.fk_user_id where fk_article_id = ? ';
+        sql = mysql.format(sql,[idArticle]);
+        return new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject({error: err});
+                resolve(result)
+            })
+        }).then((result)=>{
+            return result
+        })
+    }
+    delete(idArticle){
+        let sql = 'DELETE FROM commentaire WHERE fk_article_id = ? ';
+        sql = mysql.format(sql,idArticle._id);
+        return new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject({error: err});
+                resolve(result)
+            })
+        }).then((result)=>{
+            return result
+        })
+    }
 }
 module.exports = CommentaireModels;
--- routes/commentaire.js
+++ routes/commentaire.js
@@ -5,6 +5,8 @@ const auth = require('../middleware/auth');
 
 const commentaireCtrl = require('../controllers/commentaire');
 
+
+router.get('/', auth,commentaireCtrl.getAllCommentaire);
 router.post('/', auth,commentaireCtrl.createCommentaire);
 
 module.exports = router;
